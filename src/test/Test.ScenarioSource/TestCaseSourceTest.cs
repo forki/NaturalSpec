921755f9a497640406de5a426f1085a72bff4c00
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-02-11 14:10:43 +0100

    Renamed NaturalSpec project.

--- src/test/Test.ScenarioSource/TestCaseSourceTest.cs
+++ src/test/Test.ScenarioSource/TestCaseSourceTest.cs
@@ -3,24 +3,24 @@ using NUnit.Framework;
 
 namespace Test.ScenarioSource
 {
-[TestFixture]
-public class TestCaseSourceTest
-{
-    public IEnumerable<TestCaseData> MyTestCases()
+    [TestFixture]
+    public class TestCaseSourceTest
     {
-        return
-            new List<TestCaseData>
-                {
-                    new TestCaseData(1),
-                    new TestCaseData(2),
-                    new TestCaseData(3)
-                };
-    }
+        public IEnumerable<TestCaseData> MyTestCases()
+        {
+            return
+                new List<TestCaseData>
+                    {
+                        new TestCaseData(1),
+                        new TestCaseData(2),
+                        new TestCaseData(3)
+                    };
+        }
 
-    [TestCaseSource("MyTestCases")]
-    public void CanRunTestCaseSource(int n)
-    {
-        Assert.IsTrue(n < 10);
+        [TestCaseSource("MyTestCases")]
+        public void CanRunTestCaseSource(int n)
+        {
+            Assert.IsTrue(n < 10);
+        }
     }
-}
 }
