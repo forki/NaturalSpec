f61519d25f147d0fc5bf89611c8406d748eefb13
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-02-11 13:36:16 +0100

    Converting build script.

--- build.fsx
+++ build.fsx
@@ -11,13 +11,6 @@ let docDir = @".\Doc\"
 let deployDir = @".\deploy\"
 let testDir = @".\test\"
 let nunitPath = @".\Tools\NUnit"
-let fxCopRoot =
-  let r = environVar "FXCOPROOT"
-  if r <> "" && r <> null then r else 
-  findFile [
-    @"c:\Programme\Microsoft FxCop 1.36\"; 
-    @"c:\Program Files\Microsoft FxCop 1.36\";
-    @"c:\Program Files (x86)\Microsoft FxCop 1.36\"] "FxCopCmd.exe"
 
 // files
 let AppReferences  = !+ @"src\app\**\*.*proj"  |> Scan
@@ -33,109 +26,79 @@ let outputAssemblies =
     |> Scan
     |> Seq.map FullName
 
+let testAssemblies = 
+  !+ (testDir + @"\Spec.*.dll")     
+    ++ (testDir + @"\KnightMoves.dll")
+      |> Scan      
+
 // Targets
-Target "Clean" (fun () -> 
-  CleanDir buildDir
-  CleanDir deployDir
-  CleanDir testDir
-  CleanDir docDir
-)
+Target? Clean <-
+    fun _ -> CleanDirs [buildDir; deployDir; testDir; docDir]
 
-Target "BuildApp" (fun () -> 
-  if not isLocalBuild then
-    AssemblyInfo 
-     (fun p -> 
-        {p with
-           CodeLanguage = FSharp;
-           AssemblyVersion = buildVersion;
-           AssemblyTitle = "NaturalSpec";
-           AssemblyDescription = "NaturalSpec is a .NET UnitTest framework which provides automatically testable specs in natural language.";
-           Guid = "62F3EDB4-1ED9-415c-A349-510DF60380B5";
-           OutputFileName = @".\src\app\NaturalSpecLib\AssemblyInfo.fs"})                      
+Target? BuildApp <-
+    fun _ -> 
+        if not isLocalBuild then
+          AssemblyInfo 
+           (fun p -> 
+              {p with
+                 CodeLanguage = FSharp;
+                 AssemblyVersion = buildVersion;
+                 AssemblyTitle = "NaturalSpec";
+                 AssemblyDescription = "NaturalSpec is a .NET UnitTest framework which provides automatically testable specs in natural language.";
+                 Guid = "62F3EDB4-1ED9-415c-A349-510DF60380B5";
+                 OutputFileName = @".\src\app\NaturalSpecLib\AssemblyInfo.fs"})                      
 
-  MSBuildRelease buildDir "Build" AppReferences
-    |> Log "AppBuild-Output: "
-)
+        MSBuildRelease buildDir "Build" AppReferences
+          |> Log "AppBuild-Output: "
 
-Target "BuildTest" (fun () -> 
-  MSBuildDebug testDir "Build" TestReferences
-    |> Log "TestBuild-Output: "
-)
+Target? BuildTest <-
+    fun _ -> 
+        MSBuildDebug testDir "Build" TestReferences
+          |> Log "TestBuild-Output: "
 
-Target "Test" (fun () ->  
-  let testAssemblies = 
-    !+ (testDir + @"\Spec.*.dll")     
-      ++ (testDir + @"\KnightMoves.dll")
-      |> Scan
-      
-  let output = testDir + @"TestResults.xml"
-  NUnit (fun p -> 
-      {p with 
-         ToolPath = nunitPath; 
-         DisableShadowCopy = true; 
-         OutputFile = output}) 
-    testAssemblies  
-)
+Target? Test <-
+    fun _ ->           
+        NUnit (fun p -> 
+          {p with 
+             ToolPath = nunitPath; 
+             DisableShadowCopy = true; 
+             OutputFile = testDir + @"TestResults.xml"}) 
+          testAssemblies  
 
-Target "GenerateDocumentation" (fun () ->
-  let tool = 
-    findFile [
-      @"c:\Program Files (x86)\FSharp-1.9.7.8\bin\"; 
-      @"c:\Program Files\FSharp-1.9.7.8\bin\";
-      @"c:\Programme\FSharp-1.9.7.8\bin\"] "fshtmldoc.exe"
+Target? GenerateDocumentation <-
+    fun _ ->
+        Docu (fun p ->
+            {p with
+               ToolPath = @".\tools\FAKE\docu.exe"
+               TemplatesPath = @".\tools\FAKE\templates"
+               OutputPath = docDir })
+            (buildDir + "NaturalSpec.dll")
 
-  let trimSlash (s:string) = s.TrimEnd('\\')
-  Copy docDir [@".\HelpInput\msdn.css"]
-  let commandLineBuilder =
-    new System.Text.StringBuilder()
-      |> appendFileNamesIfNotNull  outputAssemblies
-      |> append (sprintf "--outdir\" \"%s" (docDir |> FullName |> trimSlash)) 
-      |> append "--cssfile\" \"msdn.css" 
-      |> append "--namespacefile\" \"namespaces.html" 
-  
-  trace (commandLineBuilder.ToString())
-  if not (execProcess3 (fun info ->  
-    info.FileName <- tool
-    info.WorkingDirectory <- docDir
-    info.Arguments <- commandLineBuilder.ToString()))
-  then
-    failwith "Documentation generation failed."
-)
+Target? BuildZip <-
+    fun _ ->
+        let artifacts = !+ (buildDir + "\**\*.*") -- "*.zip" |> Scan
+        let zipFileName = deployDir + sprintf "%s-%s.zip" projectName buildVersion
+        Zip buildDir zipFileName artifacts
 
-Target "FXCop" (fun () ->      
-  FxCop 
-    (fun p -> 
-      {p with 
-        ReportFileName = testDir + "FXCopResults.html";
-        ToolPath = fxCopRoot})
-    outputAssemblies
-)
+Target? ZipDocumentation <-
+    fun _ ->    
+        let docFiles = 
+          !+ (docDir + @"\**\*.*")
+            |> Scan
+        let zipFileName = deployDir + sprintf "Documentation-%s.zip" buildVersion
+        Zip @".\Doc\" zipFileName docFiles
 
-Target "BuildZip" (fun () ->
-  let artifacts = !+ (buildDir + "\**\*.*") -- "*.zip" |> Scan
-  let zipFileName = deployDir + sprintf "%s-%s.zip" projectName buildVersion
-  Zip buildDir zipFileName artifacts
-)
-
-Target "ZipDocumentation" (fun () ->    
-  let docFiles = 
-    !+ @"Doc\**\*.*"  
-      |> Scan
-  let zipFileName = deployDir + sprintf "Documentation-%s.zip" buildVersion
-  Zip @".\Doc\" zipFileName docFiles
-)
-
-Target "Default" DoNothing
-Target "Deploy" DoNothing
+Target? Default <- DoNothing
+Target? Deploy <- DoNothing
 
 // Dependencies
-"BuildApp" <== ["Clean"]
-"Test"     <== ["BuildApp"; "BuildTest"]
-"GenerateDocumentation" <== ["BuildApp"]
-"ZipDocumentation" <== ["GenerateDocumentation"]
-"BuildZip" <== ["Test"]
-"Deploy"   <== ["BuildZip"; "ZipDocumentation"]
-"Default"  <== ["Deploy"]
+For? BuildApp <- Dependency? Clean
+For? Test <- Dependency? BuildApp |> And? BuildTest
+For? GenerateDocumentation <- Dependency? BuildApp
+For? ZipDocumentation <- Dependency? GenerateDocumentation
+For? BuildZip <- Dependency? Test
+For? Deploy <- Dependency? BuildZip |> And? ZipDocumentation
+For? Default <- Dependency? Deploy
 
 // start build
-run "Default"
+Run? Default
