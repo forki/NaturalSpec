2a893eaef2ba1a7c5774cc440a3fa0576d8bc050
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-02-16 15:56:56 +0100

    Simplyfing samples.

---
+++ src/test/Spec.NaturalSpec/Factorial.fs
@@ -0,0 +1,30 @@
+﻿module NaturalSpec.Factorial
+
+let factorial x = 
+  printMethod ""  ///  prints the method name via reflection
+  let rec fac_ x =
+    match x with
+    | 0 -> 1
+    | x -> x * fac_ (x - 1)
+  fac_ x
+
+[<Scenario>]
+let When_calculating_fac_5_it_should_equal_120() =
+  Given 5
+    |> When calculating factorial
+    |> It should equal 120
+    |> Verify    
+    
+[<Scenario>]
+let When_calculating_fac_1_it_should_equal_1() =
+  Given 1
+    |> When calculating factorial
+    |> It should equal 1
+    |> Verify          
+    
+[<Scenario>]
+let When_calculating_fac_0_it_should_equal_0() =
+  Given 0
+    |> When calculating factorial
+    |> It should equal 1
+    |> Verify    
---
+++ src/test/Spec.NaturalSpec/RaisingExceptions.fs
@@ -0,0 +1,12 @@
+﻿module NaturalSpec.RaisingExceptions
+
+let raising x =
+  printfn "raising exception %A" x
+  failwith x     
+    
+[<Scenario>]
+[<FailsWith "My error">]
+let When_raising_exception() =
+  Given 0
+    |> When raising "My error"
+    |> Verify
