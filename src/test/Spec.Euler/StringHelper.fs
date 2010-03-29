df72abe39313febf9d69246c52032ec6e5ff4d07
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-03-29 15:00:39 +0200

    Adding problem 8.

--- src/test/Spec.Euler/StringHelper.fs
+++ src/test/Spec.Euler/StringHelper.fs
@@ -4,3 +4,5 @@ module StringHelper
 let isPalindrome xs = 
   let list = Seq.toList xs
   List.rev list = list
+
+let toIntList (s:string) = [for x in s -> System.Int32.Parse(x.ToString())]              
