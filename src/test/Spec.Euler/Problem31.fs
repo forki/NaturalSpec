15fea6f8a11606bdf992c836e36040b9f81e3f88
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-03-29 19:29:28 +0200

    Adding problem 31.

--- src/test/Spec.Euler/Problem31.fs
+++ src/test/Spec.Euler/Problem31.fs
@@ -3,7 +3,8 @@
 open NaturalSpec
 
 // Problem 31
-// In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+// In England the currency is made up of pound, £, and pence, p, 
+// and there are eight coins in general circulation:
 // 
 // 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
 // 
