e7b846784b03993e075ad0a4db9f38710e2475a0
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-04-24 16:46:01 +0200

    Adding Kata FizzBuzz including all test cases.

--- src/test/Spec.KataFizzBuzz/Specs.fs
+++ src/test/Spec.KataFizzBuzz/Specs.fs
@@ -3,8 +3,10 @@
 open NaturalSpec
 
 let fizzbuzz n =
-    if n % 3 = 0 then "fizz" else
-    sprintf "%d" n
+    let s1 = if n % 3 = 0 then "fizz" else ""
+    let s2 = if n % 5 = 0 then "buzz" else ""
+    let s = s1 + s2
+    if s = "" then sprintf "%d" n else s
 
 let fizzBuzz n =
   printMethod n
@@ -14,6 +16,16 @@ let fizzBuzz n =
 [<ScenarioTemplate(2,"2")>]  
 [<ScenarioTemplate(3,"fizz")>]
 [<ScenarioTemplate(4,"4")>]
+[<ScenarioTemplate(5,"buzz")>]
+[<ScenarioTemplate(6,"fizz")>]
+[<ScenarioTemplate(7,"7")>]
+[<ScenarioTemplate(8,"8")>]
+[<ScenarioTemplate(9,"fizz")>]
+[<ScenarioTemplate(10,"buzz")>]
+[<ScenarioTemplate(15,"fizzbuzz")>]
+[<ScenarioTemplate(33,"fizz")>]
+[<ScenarioTemplate(34,"34")>]
+[<ScenarioTemplate(60,"fizzbuzz")>]
 let ``Given n gives result.`` (n,result) =   
     Given n
       |> When fizzBuzz
