9060ac813241ac3efa984ef31b85862e6b1257ab
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-04-24 16:38:52 +0200

    Adding Kata FizzBuzz including first test cases.

---
+++ src/test/Spec.KataFizzBuzz/Specs.fs
@@ -0,0 +1,24 @@
+﻿module FizzBuzz.Specs
+
+open NaturalSpec
+
+let fizzbuzz n =
+  sprintf "%d" n
+
+let fizzBuzz n =
+  printMethod n
+  fizzbuzz n
+
+[<Scenario>]     
+let ``Given 1 gives 1.`` () =   
+    Given 1
+      |> When fizzBuzz
+      |> It should equal "1"
+      |> Verify
+
+[<Scenario>]     
+let ``Given 2 gives 2.`` () =   
+    Given 2
+      |> When fizzBuzz
+      |> It should equal "2"
+      |> Verify
