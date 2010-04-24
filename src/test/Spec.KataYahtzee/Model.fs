f4134d0fa325350cdf4339f4dfb6bff8136aac17
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-04-24 15:06:12 +0200

    Adding Kata Yatzee including first test case.

---
+++ src/test/Spec.KataYahtzee/Model.fs
@@ -0,0 +1,30 @@
+﻿module Yahtzee.Model
+
+type Roll = int * int * int * int * int
+
+type Category =
+| Ones
+| Twos
+| Threes
+| Fours
+| Fives
+| Sixes
+
+let toList (roll:Roll) =
+    let a,b,c,d,e = roll
+    [a;b;c;d;e]
+
+let sumNumber number =
+    Seq.filter ((=) number)
+      >> Seq.sum
+
+
+let calcValue category roll =
+    let list = toList roll
+    match category with
+    | Ones   -> sumNumber 1 list
+    | Twos   -> sumNumber 2 list
+    | Threes -> sumNumber 3 list
+    | Fours  -> sumNumber 4 list
+    | Fives  -> sumNumber 5 list
+    | Sixes  -> sumNumber 6 list
---
+++ src/test/Spec.KataYahtzee/Specs.fs
@@ -0,0 +1,26 @@
+﻿module Yahtzee.Specs
+
+open NaturalSpec
+open Model
+
+let placed_on category =
+    printMethod category
+    calcValue category 
+
+// Ones, Twos, Threes, Fours, Fives, Sixes: 
+// The player scores the sum of the dice that reads one, two, three, four, five or six, respectively. 
+// For example, 1, 1, 2, 4, 4 placed on "fours" gives 8 points.
+
+[<Scenario>]     
+let ``Given  1, 1, 2, 4, 4 placed on "fours" gives 8 points.`` () =   
+    Given (1, 1, 2, 4, 4)
+      |> When (placed_on Fours)
+      |> It should equal 8
+      |> Verify
+
+[<Scenario>]     
+let ``Given  1, 1, 6, 4, 6 placed on "sixes" gives 12 points.`` () =   
+    Given (1, 1, 6, 4, 6)
+      |> When (placed_on Sixes)
+      |> It should equal 12
+      |> Verify
