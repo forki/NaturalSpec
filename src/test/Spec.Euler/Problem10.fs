33b07f61448013f700d5b2fe3b18860263d60fda
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-04-30 17:50:37 +0200

    Adding solution to Euler problem 10.

---
+++ src/test/Spec.Euler/Problem10.fs
@@ -0,0 +1,28 @@
+﻿module Problem10
+  
+open NaturalSpec
+
+// Problem 10  
+// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+//
+// Find the sum of all the primes below two million.
+
+let FindSumOfPrimes n =
+    printMethod ""
+    primes n  
+      |> List.sum 
+
+[<Scenario>]      
+let ``Find the sum of all the primes below 10``() =
+    Given 10I
+      |> When solving FindSumOfPrimes
+      |> It should equal 17I
+      |> Verify
+
+
+[<Scenario>]      
+let ``Find the sum of all the primes below two million``() =
+    Given 2000000I
+      |> When solving FindSumOfPrimes
+      |> It should equal 142913828922I
+      |> Verify
