96bb8bab90ebca09e303383f5704e0efb438385f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-05-05 12:52:26 +0200

    Adding solution to Euler problem 16.

--- src/test/Spec.Euler/MathHelper.fs
+++ src/test/Spec.Euler/MathHelper.fs
@@ -66,6 +66,12 @@ let factors n =
        let j = n / i
        if i <> j then yield j]
 
+let pow a b = System.Numerics.BigInteger.Pow(a,b) 
+
 let factorial n = seq { 1I..n } |> Seq.fold (*) 1I
 
 let binomial n k = factorial n / (factorial k * factorial(n-k))
+
+let rec sum_of_digits n =
+    if n = 0I then 0I else
+    n % 10I + sum_of_digits (n / 10I)
---
+++ src/test/Spec.Euler/Problem16.fs
@@ -0,0 +1,26 @@
+﻿module Problem16
+  
+open NaturalSpec
+
+// Problem 16
+// 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+//
+// What is the sum of the digits of the number 2^1000?
+
+let SumOfDigits n =
+    printMethod ""
+    sum_of_digits n
+
+[<Scenario>]      
+let ``What is the sum of the digits of the number 2^15?``() =
+    Given (pow 2I 15)
+      |> When solving SumOfDigits
+      |> It should equal 26I
+      |> Verify
+
+[<Scenario>]      
+let ``What is the sum of the digits of the number 2^1000?``() =
+    Given (pow 2I 1000)
+      |> When solving SumOfDigits
+      |> It should equal 1366I
+      |> Verify
