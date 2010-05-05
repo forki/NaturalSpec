9cbf406e876e9f04c3d92572a335523b8558c798
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-05-05 13:03:00 +0200

    Adding solution to Euler problem 19.

---
+++ src/test/Spec.Euler/Problem19.fs
@@ -0,0 +1,29 @@
+﻿module Problem19
+
+open NaturalSpec
+
+// Problem 19
+// How many Sundays fell on the first of the month during the twentieth century 
+// (1 Jan 1901 to 31 Dec 2000)?
+
+let date d m y  = new System.DateTime(y,m,d)
+
+let dates (startDate:System.DateTime) endDate = 
+    let actDate = ref startDate
+    seq {
+        while !actDate <= endDate do
+            yield !actDate
+            actDate := (!actDate).AddDays(1.0) }
+
+let CountFirstSundaysUntil endDate startDate =
+    printMethod endDate
+    dates startDate endDate
+      |> Seq.filter (fun d -> d.DayOfWeek = System.DayOfWeek.Sunday && d.Day = 1) 
+      |> Seq.length
+
+[<Scenario>]      
+let ``How many Sundays fell on the first of the month during the twentieth century?``() =
+    Given (date 1 1 1901)
+      |> When solving (CountFirstSundaysUntil (date 31 12 2000))
+      |> It should equal 171
+      |> Verify
