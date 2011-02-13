229bb0485734a7458a9fdc68beb1d4cdb5b5a034
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-02-13 12:42:28 +0100

    Cleanup

--- src/test/Spec.Euler/Memoization.fs
+++ src/test/Spec.Euler/Memoization.fs
@@ -4,12 +4,12 @@ module Memoization
 open System.Collections.Generic
 
 let memoizeWithDictionary f =
-  let cache = Dictionary<_, _>()
-  fun x ->
-    let (found,result) = cache.TryGetValue(x)
-    if found then
-      result
-    else
-      let res = f x
-      cache.[x] <- res
-      res
+    let cache = Dictionary<_, _>()
+    fun x ->
+        let (found,result) = cache.TryGetValue(x)
+        if found then
+            result
+        else
+            let res = f x
+            cache.[x] <- res
+            res
