01f779919937dc0d4325f2ac2a67456c1d3b47e0
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-02-13 13:19:07 +0100

    Cleanup KataTennis

--- src/test/Spec.KataTennis/Specs.fs
+++ src/test/Spec.KataTennis/Specs.fs
@@ -3,7 +3,11 @@
 open NaturalSpec
 open Model
 
+let Player1 = 1
+let Player2 = 2
+
 let point_goes_to player game =
+    let player = Player.FromInt player
     printMethod player
     score game player
 
@@ -16,76 +20,76 @@ let ``A newly started game should start with score Love to Love`` () =
 [<Scenario>]     
 let ``When Player1 scores once the score should be Fifteen to Love`` () =   
     Given NewGame
-      |> When point_goes_to Player.Player1
+      |> When point_goes_to Player1
       |> It should equal (Fifteen <=> Love)
       |> Verify
 
 [<Scenario>]     
 let ``When player one scores twice the score should be Thirty to Love`` () =   
     Given NewGame
-      |> When point_goes_to Player.Player1
-      |> When point_goes_to Player.Player1
+      |> When point_goes_to Player1
+      |> When point_goes_to Player1
       |> It should equal (Thirty <=> Love)
       |> Verify
 
 [<Scenario>]     
 let ``When player two scores 3 times the score should be Love to Fourty`` () =   
     Given NewGame
-      |> When point_goes_to Player.Player2
-      |> When point_goes_to Player.Player2
-      |> When point_goes_to Player.Player2
+      |> When point_goes_to Player2
+      |> When point_goes_to Player2
+      |> When point_goes_to Player2
       |> It should equal (Love <=> Fourty)
       |> Verify
 
-[<Example(Player.Player1)>]  
-[<Example(Player.Player2)>]  
+[<Example(1)>]  
+[<Example(2)>]  
 let ``If a player scores 4 times and the other did not score he has won`` winner =
     Given NewGame
       |> When point_goes_to winner
       |> When point_goes_to winner
       |> When point_goes_to winner
       |> When point_goes_to winner
-      |> It should equal (Victory winner)
+      |> It should equal (Victory <| Player.FromInt winner)
       |> Verify
 
-[<Example(Player.Player1)>]  
-[<Example(Player.Player2)>]  
+[<Example(1)>]  
+[<Example(2)>]  
 let ``When a player scores after a deuce he should have the advantage`` player =
     Given Deuce
       |> When point_goes_to player
-      |> It should equal (Advantage player)
+      |> It should equal (Advantage <| Player.FromInt player)
       |> Verify
 
 [<Scenario>] 
 let ``If both players score 4 times the game should be deuce``() =
-    Given (Thirty <=> Thirty)
-      |> When point_goes_to Player.Player1
-      |> When point_goes_to Player.Player2
+    Given (Fourty <=> Fourty)
+      |> When point_goes_to Player1
+      |> When point_goes_to Player2
       |> It should equal Deuce
       |> Verify
 
-[<Example(Player.Player1)>]  
-[<Example(Player.Player2)>]  
+[<Example(1)>]  
+[<Example(2)>]  
 let ``When a player scores after he got advantage he has won`` player =
-    Given (Advantage player)
+    Given (Advantage <| Player.FromInt player)
       |> When point_goes_to player
-      |> It should equal (Victory player)
+      |> It should equal (Victory <| Player.FromInt player)
       |> Verify
 
-[<Example(Player.Player1,Player.Player2)>]  
-[<Example(Player.Player2,Player.Player1)>]  
+[<Example(1,2)>]  
+[<Example(2,1)>]  
 let ``When a player looses his advantage they should be back to deuce`` player opponent =
-    Given (Advantage player)
+    Given (Advantage <| Player.FromInt player)
       |> When point_goes_to opponent
       |> It should equal Deuce
       |> Verify
 
-[<Example(Player.Player1,Player.Player1)>]  
-[<Example(Player.Player1,Player.Player2)>]  
-[<Example(Player.Player2,Player.Player1)>]  
-[<Example(Player.Player2,Player.Player2)>]  
+[<Example(1,1)>]  
+[<Example(1,2)>]  
+[<Example(2,1)>]  
+[<Example(2,2)>]  
 let ``A won game is not changable`` player scorer =
-    Given (Victory player)
+    Given (Victory <| Player.FromInt player)
       |> When point_goes_to scorer
-      |> It should equal (Victory player)
+      |> It should equal (Victory <| Player.FromInt player)
       |> Verify
--- src/test/Spec.KataTennis/Tennis.fs
+++ src/test/Spec.KataTennis/Tennis.fs
@@ -6,8 +6,11 @@ let Thirty = 2
 let Fourty = 3
 
 type Player =
-| Player1 = 0
-| Player2 = 1
+| Player1 
+| Player2
+  static member FromInt = function
+    | 1 -> Player1
+    | _ -> Player2
 
 type Game = 
 | OpenGame of int * int
@@ -22,21 +25,21 @@ let NewGame = OpenGame(Love,Love)
 let getScore = function 
 | OpenGame(x,y) -> x,y
 | Deuce -> 4,4
-| Advantage player when player = Player.Player1 -> 5,4 
-| Advantage player when player = Player.Player2 -> 4,5
-| Victory player when player = Player.Player1 -> 5,0
-| Victory player when player = Player.Player2 -> 0,5
+| Advantage Player1 -> 5,4 
+| Advantage Player2 -> 4,5
+| Victory Player1 -> 5,0
+| Victory Player2 -> 0,5
 
 let score game player =     
     let oldScore = getScore game
     let newScore =
         match player with
-        | Player.Player1 -> fst oldScore + 1,snd oldScore
-        | Player.Player2 -> fst oldScore,snd oldScore + 1
+        | Player1 -> fst oldScore + 1,snd oldScore
+        | Player2 -> fst oldScore,snd oldScore + 1
     match newScore with
     | x,y when x > Fourty && y > Fourty && x = y -> Deuce
-    | x,y when x > Fourty && x-y = 1 -> Advantage Player.Player1
-    | x,y when y > Fourty && y-x = 1 -> Advantage Player.Player2
-    | x,y when x > Fourty && x > y-> Victory Player.Player1
-    | x,y when y > Fourty && y > x-> Victory Player.Player2
-    | _ -> OpenGame newScore
+    | x,y when x > Fourty && x-y = 1             -> Advantage Player1
+    | x,y when y > Fourty && y-x = 1             -> Advantage Player2
+    | x,y when x > Fourty && x > y               -> Victory Player1
+    | x,y when y > Fourty && y > x               -> Victory Player2
+    | _                                          -> OpenGame newScore
