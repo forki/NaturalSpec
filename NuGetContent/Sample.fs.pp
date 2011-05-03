3273f72420bbcd77387ddb4b5145121fd79843d2
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-05-03 09:02:39 +0200

    Fixing sample.

--- NuGetContent/Sample.fs.pp
+++ NuGetContent/Sample.fs.pp
@@ -5,7 +5,7 @@ open NaturalSpec
 [<Scenario>]
 let ``After removing 3 from a list it should not contain 3``() =
   Given [1;2;3;4;5]              // Arrange test context
-    |> When Removing 3           // Act
+    |> When removing 3           // Act
     |> It shouldn't contain 3    // Assert
     |> It should have (length 4) // Assert      
     |> It should contain 4       // Assert
