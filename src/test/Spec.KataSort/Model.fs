16049b0f484a909626de1569ae78256685d01a4a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-02-04 21:05:02 +0100

    Starting Kata.Sort - sorting three elements - still simplifying

--- src/test/Spec.KataSort/Model.fs
+++ src/test/Spec.KataSort/Model.fs
@@ -2,10 +2,8 @@
 
 let rec sort list = 
     match list with
-    | [] -> list
-    | [x] -> list    
-    | [x;y] -> if x<y then [x;y] else [y;x]
-    | [x;y;z] ->
+    | [] -> list 
+    | x::rest ->
          sort (List.filter (fun e -> e < x) list) @ 
          [x] @ 
          sort (List.filter (fun e -> e > x) list)    
