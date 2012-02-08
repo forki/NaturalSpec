dbbc66ca29e94f553e04504aeb34f9bb25089c7c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-02-08 18:13:52 +0100

    KataKataNumberToLCD - display single numbers

--- src/test/Spec.KataNumberToLCD/Model.fs
+++ src/test/Spec.KataNumberToLCD/Model.fs
@@ -1,5 +1,33 @@
 ﻿module KataNumberToLCD.Model
 
+type Segment =
+| Blank
+| Right
+| Left
+| Both
+| Middle
+
+let segmentToString = function
+| Blank    -> "   "
+| Right    -> "  |"
+| Left     -> "|  "
+| Both     -> "| |"
+| Middle   -> " - "
+
+
 let toLCD number =
-    if number = "1" then "   \n  |\n  |\n" else
-     " _ \n| |\n|_|\n"
+    let segments =
+        match number with
+        | 1 -> [Blank; Right; Blank; Right; Blank]
+        | 2 -> [Middle; Right; Middle; Left; Middle]
+        | 3 -> [Middle; Right; Middle; Right; Middle]
+        | 4 -> [Blank; Both; Middle; Right; Blank]
+        | 5 -> [Middle; Left; Middle; Right; Middle]
+        | 6 -> [Middle; Left; Middle; Both; Middle]
+        | 7 -> [Middle; Right; Blank; Right; Blank]
+        | 8 -> [Middle; Both; Middle; Both; Middle]
+        | 9 -> [Middle; Both; Middle; Right; Middle]
+        | _ -> [Middle; Both; Blank; Both; Middle]
+    segments 
+    |> Seq.map segmentToString
+    |> Seq.fold (fun s x -> s + x + "\n") ""
--- src/test/Spec.KataNumberToLCD/Specs.fs
+++ src/test/Spec.KataNumberToLCD/Specs.fs
@@ -10,20 +10,120 @@ let converting_to_LCD number =
 
 [<Scenario>]     
 let ``Should display single 0`` () =   
-    Given "0"
+    Given 0
       |> When converting_to_LCD
       |> It should equal 
-            (" _ \n" +
+            (" - \n" +
              "| |\n" +
-             "|_|\n")
+             "   \n" +
+             "| |\n" +
+             " - \n")
       |> Verify
 
 [<Scenario>]     
 let ``Should display single 1`` () =   
-    Given "1"
+    Given 1
       |> When converting_to_LCD
       |> It should equal 
             ("   \n" +
              "  |\n" +
-             "  |\n")
+             "   \n" +
+             "  |\n" +
+             "   \n")
+      |> Verify
+
+[<Scenario>]     
+let ``Should display single 2`` () =   
+    Given 2
+      |> When converting_to_LCD
+      |> It should equal 
+            (" - \n" +
+             "  |\n" +
+             " - \n" +
+             "|  \n" +
+             " - \n")
+      |> Verify
+
+[<Scenario>]     
+let ``Should display single 3`` () =   
+    Given 3
+      |> When converting_to_LCD
+      |> It should equal 
+            (" - \n" +
+             "  |\n" +
+             " - \n" +
+             "  |\n" +
+             " - \n")
+      |> Verify
+
+[<Scenario>]     
+let ``Should display single 4`` () =   
+    Given 4
+      |> When converting_to_LCD
+      |> It should equal 
+            ("   \n" +
+             "| |\n" +
+             " - \n" +
+             "  |\n" +
+             "   \n")
+      |> Verify
+
+[<Scenario>]     
+let ``Should display single 5`` () =   
+    Given 5
+      |> When converting_to_LCD
+      |> It should equal 
+            (" - \n" +
+             "|  \n" +
+             " - \n" +
+             "  |\n" +
+             " - \n")
+      |> Verify
+
+[<Scenario>]     
+let ``Should display single 6`` () =   
+    Given 6
+      |> When converting_to_LCD
+      |> It should equal 
+            (" - \n" +
+             "|  \n" +
+             " - \n" +
+             "| |\n" +
+             " - \n")
+      |> Verify
+
+[<Scenario>]     
+let ``Should display single 7`` () =   
+    Given 7
+      |> When converting_to_LCD
+      |> It should equal 
+            (" - \n" +
+             "  |\n" +
+             "   \n" +
+             "  |\n" +
+             "   \n")
+      |> Verify
+
+[<Scenario>]     
+let ``Should display single 8`` () =   
+    Given 8
+      |> When converting_to_LCD
+      |> It should equal 
+            (" - \n" +
+             "| |\n" +
+             " - \n" +
+             "| |\n" +
+             " - \n")
+      |> Verify
+
+[<Scenario>]     
+let ``Should display single 9`` () =   
+    Given 9
+      |> When converting_to_LCD
+      |> It should equal 
+            (" - \n" +
+             "| |\n" +
+             " - \n" +
+             "  |\n" +
+             " - \n")
       |> Verify
