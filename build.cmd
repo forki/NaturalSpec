c9d5e1ae5e784f6efa19e95b63ff5c4851c1051f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-07-31 09:20:40 +0200

    Nuget packages updated.

--- build.cmd
+++ build.cmd
@@ -7,7 +7,7 @@ SET TARGET="Default"
 
 IF NOT [%1]==[] (set TARGET="%1")
   
-"packages\FAKE.1.62.1\tools\Fake.exe" "build.fsx" "target=%TARGET%"
+"packages\FAKE.1.64.7\tools\Fake.exe" "build.fsx" "target=%TARGET%"
 
 rem Bail if we're running a TeamCity build.
 if defined TEAMCITY_PROJECT_NAME goto Quit
